4636649518ec301f65533320e514cf888f69df2c
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2022-01-28 19:07:06 -0800

    files read
    need to process file values

--- a-star.py
+++ a-star.py
@@ -1,1 +1,16 @@
+# A∗ search
+# ▪Idea: avoid expanding paths that are already expensive
+# ▪Evaluation function 𝑓(𝑛) = 𝑔(𝑛) + ℎ(𝑛)
+# ▪𝑔(𝑛) = cost so far to reach 𝑛
+# ▪ℎ(𝑛) = estimated cost to goal from 𝑛
+# ▪𝑓(𝑛) = estimated total cost of path through 𝑛 to goal
+print('hi')
 
+with open('map.txt') as f:
+    map_text = f.readlines()
+
+with open('coordinates.txt') as f:
+    coordinates = f.readlines()
+
+print(map_text)
+print(coordinates)
