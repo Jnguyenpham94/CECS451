2f12f74ef62fe23c775264f3e96e74a2b95eca3d
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2022-01-30 19:39:24 -0800

    coordinates.txt processed next map.txt

--- a-star.py
+++ a-star.py
@@ -6,7 +6,47 @@
 # ▪𝑓(𝑛) = estimated total cost of path through 𝑛 to goal
 import math
 
-print('hi')
+
+# convert straight line distance between 2 points
+def haversine(deg1, deg2):
+    lat1 = math.radians(float(deg1))
+    lat2 = math.radians(float(deg2))
+    r = 3958.8  # miles
+    d = 2 * r * math.asin(math.sqrt(math.sin((lat2 - lat1) / 2) ** 2) + math.cos(lat1) * math.cos(lat2) * math.sin(
+        (lat2 - lat1) / 2) ** 2)
+    # print(lat1)
+    # print(lat2)
+    # print(d)
+    return d
+
+
+try:
+    map_text = open("map.txt").read().strip()
+# print(map_text)
+except FileNotFoundError:
+    print("File Not Found")
+    exit(1)
+
+# parse coordinates.txt to usable values
+try:
+    coordinates_text = open("coordinates.txt").read()
+    remove_chars = ":,\n()"
+    for chars in remove_chars:
+        coordinates_text = coordinates_text.replace(chars, " ")
+    coordinates_text = coordinates_text.split()
+    distance = {}
+    latitude = 1
+    for x in range(len(coordinates_text)):
+        try:
+            distance[coordinates_text[x]] = haversine(coordinates_text.pop(latitude), coordinates_text.pop(latitude))
+            latitude += 1
+        except IndexError:
+            break
+    print(distance)
+    # print(coordinates_text)
+except FileNotFoundError:
+    print("File Not Found")
+    exit(1)
 
 with open('map.txt') as f:
     map_text = f.readlines()
@@ -14,19 +54,7 @@ with open('map.txt') as f:
 with open('coordinates.txt') as f:
     coordinates = f.readlines()
 
+# print(map_text)
+# print(coordinates)
 
-# convert straight line distance between 2 points
-def haversine(deg1, deg2):
-    lat1 = math.radians(deg1)
-    lat2 = math.radians(deg2)
-    r = 3958.8  # miles
-    d = 2 * r * math.asin(math.sqrt(math.sin((lat2 - lat1) / 2) ** 2) + math.cos(lat1) * math.cos(lat2) * math.sin(
-        (lat2 - lat1) / 2) ** 2)
-    print(lat1)
-    print(lat2)
-
-
-print(map_text)
-print(coordinates)
-
-haversine(37.38305013, -121.8734782)
+# haversine(37.38305013, -121.8734782)
