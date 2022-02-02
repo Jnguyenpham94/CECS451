be358926b58d97fbe4c834e9731d8a0147148f2a
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2022-02-01 22:21:02 -0800

    included basic node stuff for map nodes
    need to figure out how to load all the values in

--- a-star.py
+++ a-star.py
@@ -9,6 +9,15 @@ import math
 import sys
 
 
+class Node(object):
+    def __init__(self, data):
+        self.distance = data
+        self.children = []
+
+    def add_child(self, obj):
+        self.children.append(obj)
+
+
 # convert straight line distance between 2 points
 def haversine(latitude1, longitude1, latitude2, longigude2):
     lat1 = math.radians(float(latitude1))
@@ -29,7 +38,7 @@ except FileNotFoundError:
     print("map.txt Not Found")
     exit(1)
 
-# parse coordinates.txt to usable values
+# parse coordinates.txt to usable values in dictionary; city : latitude, longitude
 try:
     coordinates_text = open("coordinates.txt").read()
     remove_chars = ":,\n()"
@@ -55,7 +64,14 @@ except FileNotFoundError:
 def main(args):
     start = args[0]  # starting city (SanFrancisco)
     end = args[1]  # ending city (LongBeach)
-    straight_line = haversine(distance[start]["latitude"], distance[start]["longitude"], distance[end]["latitude"], distance[end]["longitude"],)
+    straight_line = haversine(distance[start]["latitude"], distance[start]["longitude"], distance[end]["latitude"], distance[end]["longitude"], )  # h(n)
+    p = Node(121)
+    p2 = Node(315)
+    p3 = Node(35)
+    p.add_child(p2)
+    p.add_child(p3)
+    for d in p.children:
+        print(d.distance)
     # print(straight_line)
     # print(start)
     # print(end)
