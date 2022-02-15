efd6759c0a04bd9701ffe32d2601e9c5fc10f548
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2022-02-14 22:12:17 -0800

    output complete need to implement algos

--- genetic.py
+++ genetic.py
@@ -1,9 +1,17 @@
 import time
 
+from board import Board
+
 start = time.time()
 
 # body statement here
+test = Board(5)
+fitness = test.get_fitness()
+while fitness > 0:
+    print("FITNESS")
 
+# body end here
 end = time.time()
 
-print(f"Runtime of the program is {end - start}")
+print(f"Runtime of the program is {end - start:.2f}")
+test.show_map()
--- hill.py
+++ hill.py
@@ -1,9 +1,18 @@
 import time
 
+from board import Board
+
 start = time.time()
 
 # body statement here
+test = Board(5)
+fitness = test.get_fitness()
+print("FITNESS: " + str(test.get_fitness()))
+while fitness > 0:
+    print("FITNESS")
 
+# body end here
 end = time.time()
 
-print(f"Runtime of the program is {end - start}")
+print(f"Runtime of the program is {(end - start):.2f}")
+test.show_map()
