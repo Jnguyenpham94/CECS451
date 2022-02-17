f85f8888670d1603b909e6562fbad8e30f30e3b9
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2022-02-16 23:49:35 -0800

    hill in progress

--- hill.py
+++ hill.py
@@ -1,54 +1,37 @@
 import random
 import time
 
+import numpy as np
+
 from board import Board
-
-
-def make_move_steepest_hill(board):
-    moves = {}
-    for col in range(len(board)):
-        best_move = board[col]
-
-        for row in range(len(board)):
-            if board[col] == row:
-                # We don't need to evaluate the current
-                # position, we already know the h-value
-                continue
-
-            board_copy = list(board)
-            # Move the queen to the new row
-            board_copy[col] = row
-            moves[(col, row)] = board_copy.get_fitness()
-
-    best_moves = []
-    h_to_beat = board_copy.get_fitness()
-    for k, v in moves.iteritems():
-        if v < h_to_beat:
-            h_to_beat = v
-
-    for k, v in moves.iteritems():
-        if v == h_to_beat:
-            best_moves.append(k)
-
-    # Pick a random best move
-    if len(best_moves) > 0:
-        pick = random.randint(0, len(best_moves) - 1)
-        col = best_moves[pick][0]
-        row = best_moves[pick][1]
-        board[col] = row
-
-    return board
-
 
 start = time.time()
 
 # body statement here
 test = Board(5)
 fitness = test.get_fitness()
-result = make_move_steepest_hill(test)
+current_fit = fitness
+# for i in test.map[0]:
+#     print(i)
+while fitness > 0:
+    row = 0
+    for col, value in enumerate(test.map[row]):
+        if value == 1:
+            current_loc = col
+            change_loc = 0
+            while fitness >= current_fit:
+                test.map[row][current_loc] = 0
+                test.map[row][change_loc] = 1
+                current_fit = test.get_fitness()
+                if current_fit < fitness:
+                    fitness = current_fit
+                    row += 1
+                else:
+                    change_loc += 1
+# result = make_move_steepest_hill(test)
 
 # body end here
 end = time.time()
 
 print(f"Runtime of the program is {(end - start):.2f}")
-result.show_map()
+# result.show_map()
