fdb165644fae5e7642ef401d86ba1db6092ee344
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2022-02-19 13:27:44 -0800

    cleaned up code
    unnecessary comments, and n variable created to control queen number

--- hill.py
+++ hill.py
@@ -3,17 +3,15 @@ import time
 from board import Board
 
 
-def hill(board):
-    # body statement here
+def hill(board, n_queens):
     count = 0  # loop 5 times then restart board
-    n = 5
+    n = n_queens
     fitness = board.get_fitness()
     h = [0] * n  # fitness of potential board
-    current_fit = fitness
     while fitness != 0:
         for i in range(len(board.map)):
             for j in range(len(board.map[i])):
-                board.map[i] = [0] * n
+                board.map[i] = [0] * n  # remove any 1s in that row
                 board.map[i][j] = 1
                 h[j] = board.get_fitness()
                 board.map[i][j] = 0
@@ -22,19 +20,18 @@ def hill(board):
             board.map[i][min_index] = 1
             fitness = board.get_fitness()
         count += 1
-        #  if after 5 loops to bottom row restart with fresh board
+        #  if after 5 loops to bottom row w/o finding the best fitness restart with fresh board
         if count == 5:
             count = 0
-            board = Board(5)  # restart with fresh board
+            board = Board(n)  # restart with fresh board
     return board
-
-    # body end here
 
 
 def main():
     start = time.time() * 1000
-    test = Board(5)
-    test = hill(test)
+    n = 5  # CHANGE ME for more or less queens
+    test = Board(n)
+    test = hill(test, n)
     end = time.time() * 1000
     # fitness = test.get_fitness()
     # print(fitness)
