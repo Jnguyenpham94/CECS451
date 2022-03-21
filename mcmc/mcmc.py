30f65911758eccf63af6f7650b2827a1c206865d
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2022-03-20 22:35:10 -0700

    assn 8 setup

--- mcmc/mcmc.py
+++ mcmc/mcmc.py
@@ -1,6 +1,11 @@
 #  mcmc algorithm 451
 
+#  place mcmc algorithm here
+def mcmc():
+    print("mcmc")
+    
+def main():
+    print("I AM HERE")
 
-
-if __name__ == "main":
-    pass
+if __name__ == "__main__":
+    main()
