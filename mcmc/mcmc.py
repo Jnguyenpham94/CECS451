b915707af9f70cf09025d46cd6e0e1eaae241eeb
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2022-03-21 21:28:28 -0700

    output update

--- mcmc/mcmc.py
+++ mcmc/mcmc.py
@@ -1,11 +1,16 @@
 #  mcmc algorithm 451
+# (a)P (C|¬s, r), ~P (C|¬s, ¬r), ~P (R|c, ¬s, w), ~P (R|¬c, ¬s, w)
+# 
 
 #  place mcmc algorithm here
 def mcmc():
-    print("mcmc")
+    pass
     
 def main():
-    print("I AM HERE")
+    print("C|-s,r = <,>")
+    print("C|-s,-r = <,>")
+    print("R|c,-s,w = <,>")
+    print("R|-c,-s,w = <,>")
 
 if __name__ == "__main__":
     main()
