43eec3290b17c9957b4cfd1c9212b39d95b0614a
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2022-03-27 03:07:44 -0700

    part B DONE part C NEXT

--- mcmc/mcmc.py
+++ mcmc/mcmc.py
@@ -5,39 +5,31 @@
 # (c) (20 points) Show the probability of the query ~P (C|¬s, w)
 # (d) Please follow the output format. (Fix precisions using "{:.4f}".format)
 
-# Part A. The sampling probabilities
-# P(C|-s,r) = <..., ...>
-# P(C|-s,-r) = <..., ...>
-# P(R|c,-s,w) = <..., ...>
-# P(R|-c,-s,w) = <..., ...>
-
 # Part B. The transition probability matrix
 # S1 S2 S3 S4
 # S1 . . . .
 # S2 . . . .
 # S3 . . . .
 # S4 . . . .
-# Part C. The probability for the query
-# P(C|-s,w) = <..., ...>
 
    
 def main(n):
     #  .4f format
     print("Part A. The sampling probabilities")
-    print("C|-s,r = a<.8780, .1212>")  #  a = 2.4390
-    print("C|-s,-r = a<.9863, .0137>")  #  a = 1.3699
-    print("R|c,-s,w = a<.3103, .6897>")  #  a = 3.4483
-    print("R|-c,-s,w = a<.8182, .1818>")  #  a = 4.5455
+    print("C|-s,r = <.8780, .1212>")  #  a = 2.4390
+    print("C|-s,-r = <.9863, .0137>")  #  a = 1.3699
+    print("R|c,-s,w = <.3103, .6897>")  #  a = 3.4483
+    print("R|-c,-s,w = <.8182, .1818>")  #  a = 4.5455
     
     print("Part B. The transition probability matrix")
     print("\tS1\t S2\t S3\t S4")
-    print("S1\t .\t .\t .\t .\t")
-    print("S2\t .\t .\t .\t .\t")
-    print("S3\t .\t .\t .\t .\t")
-    print("S4\t .\t .\t .\t .\t")
+    print("S1\t .5400\t .0050\t .0250\t .0000\t")
+    print("S2\t .3600\t .0500\t .0000\t .0450\t")
+    print("S3\t .1800\t .0000\t .1150\t .0200\t")
+    print("S4\t .0000\t .0450\t .0900\t .0700\t")
     
     print("Part C. The probability for the query")
-    print("C|-s,r = <,>")
+    print("C|-s,w = <,>")
 
 if __name__ == "__main__":
     n = 1000000
