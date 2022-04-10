3b9d6101f619871e8db3262c7db5f553ec992b91
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2022-04-09 23:18:04 -0700

    successful read in of values

--- hmm/hmm.py
+++ hmm/hmm.py
@@ -1,9 +1,21 @@
 import sys
 
+
 # .4f format
-def main():
-    print("0.5,0.7,0.3,0.9,0.2,t,t--><0.8834,0.1166>")
-    print("0.5,0.7,0.3,0.9,0.2,t,t,f--><0.1907,0.8093>")
+def main(args):
+    try:
+        content = open(args[1], 'r').read()
+        remove_chars = ":,{}'''"
+        for chars in remove_chars:
+                content = content.replace(chars, " ")
+        content = content.split()
+    except:
+        print("File not found. Goodbye")
+        exit()
+    print(content)
+    # print("0.5,0.7,0.3,0.9,0.2,t,t--><0.8834,0.1166>")
+    # print("0.5,0.7,0.3,0.9,0.2,t,t,f--><0.1907,0.8093>")
+
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main(sys.argv)
