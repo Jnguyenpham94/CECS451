918311fda965b1ee94effdf422415f997c82f514
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2022-04-10 16:07:09 -0700

    values processed now need to implement hmm

--- hmm/hmm.py
+++ hmm/hmm.py
@@ -1,20 +1,46 @@
 import sys
 
 
+def hmm(probs, evidence):
+    x0 = (probs[0], 1 - float(probs[0]))
+    # print(probs)
+    b = (probs[1], True)
+    c = (probs[2], False)
+    d = (probs[3], True)
+    f = (probs[4], False)
+    e = [evidence[0], evidence[1]]
+    # print(e)
+    # print("0.5,0.7,0.3,0.9,0.2,t,t--><0.8834,0.1166>")
+    # print("0.5,0.7,0.3,0.9,0.2,t,t,f--><0.1907,0.8093>")
+
+
+#  ['0.5', '0.7', '0.3', '0.9', '0.2', 't', 't', '0.7', '0.8', '0.3', '0.2', '0.7', 't', 'f']
 # .4f format
 def main(args):
+    #  process probs from txt file
     try:
         content = open(args[1], 'r').read()
         remove_chars = ":,{}'''"
         for chars in remove_chars:
-                content = content.replace(chars, " ")
+            content = content.replace(chars, " ")
         content = content.split()
-    except:
+    except FileNotFoundError:
         print("File not found. Goodbye")
         exit()
-    print(content)
-    # print("0.5,0.7,0.3,0.9,0.2,t,t--><0.8834,0.1166>")
-    # print("0.5,0.7,0.3,0.9,0.2,t,t,f--><0.1907,0.8093>")
+    eValues = []
+    probs = []
+    for i in content:
+        if type(i) is str:
+            if i == 't':
+                eValues.append(True)
+            elif i == 'f':
+                eValues.append(False)
+            else:
+                probs.append(i)
+    # print(eValues)
+    # print(probs)
+    hmm(probs[0:5], (eValues[0], eValues[1]))
+    hmm(probs[5:10], (eValues[2], eValues[3]))
 
 
 if __name__ == '__main__':
