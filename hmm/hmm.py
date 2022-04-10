0fd46a17ae17faf36b2624a6e8e4fb7affc1f51e
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2022-04-09 21:50:37 -0700

    Assn 10 START

---
+++ hmm/hmm.py
@@ -0,0 +1,9 @@
+import sys
+
+# .4f format
+def main():
+    print("0.5,0.7,0.3,0.9,0.2,t,t--><0.8834,0.1166>")
+    print("0.5,0.7,0.3,0.9,0.2,t,t,f--><0.1907,0.8093>")
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
