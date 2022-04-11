eb32fdc08558297332938ffca6fb5cb0481a39e2
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2022-04-10 19:03:44 -0700

    algo basically done need to output correctly

--- hmm/hmm.py
+++ hmm/hmm.py
@@ -1,14 +1,31 @@
 import sys
 
 
+def filtering(x, b, c, d, f, e):
+    _x = (float(x[0]) * float(b[0])) + (float(x[0]) * (1 - float(b[0])))
+    _y = (float(x[1]) * (1 - float(b[0]))) + (float(x[1]) * float(b[0]))
+    _x *= float(d[0])
+    _y *= float(f[0])
+    alph = 1 / (_x + _y)
+    _x *= alph
+    _y *= alph
+    xt = (_x, _y)
+    
+    return xt
+
+
 def hmm(probs, evidence):
-    x0 = (probs[0], 1 - float(probs[0]))
+    x0 = (float(probs[0]), 1 - float(probs[0]))  # (x, not x)
     # print(probs)
     b = (probs[1], True)
     c = (probs[2], False)
     d = (probs[3], True)
     f = (probs[4], False)
     e = [evidence[0], evidence[1]]
+    x1 = filtering(x0, b, c, d, f, e)
+    x2 = filtering(x1, b, c, d, f, e)
+    #  filtering stuff below
+
     # print(e)
     # print("0.5,0.7,0.3,0.9,0.2,t,t--><0.8834,0.1166>")
     # print("0.5,0.7,0.3,0.9,0.2,t,t,f--><0.1907,0.8093>")
@@ -37,10 +54,9 @@ def main(args):
                 eValues.append(False)
             else:
                 probs.append(i)
-    # print(eValues)
-    # print(probs)
-    hmm(probs[0:5], (eValues[0], eValues[1]))
-    hmm(probs[5:10], (eValues[2], eValues[3]))
+
+    hmm(probs[0:5], (eValues[0], eValues[1]))  # line 1 from cpt.txt
+    hmm(probs[5:10], (eValues[2], eValues[3])) # line 2 from cpt.txt
 
 
 if __name__ == '__main__':
