dc0416561cb42c11dbec0e0633b24a3b517206b6
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2022-04-10 19:20:39 -0700

    output looks good need to check with TA

--- hmm/hmm.py
+++ hmm/hmm.py
@@ -6,11 +6,10 @@ def filtering(x, b, c, d, f, e):
     _y = (float(x[1]) * (1 - float(b[0]))) + (float(x[1]) * float(b[0]))
     _x *= float(d[0])
     _y *= float(f[0])
-    alph = 1 / (_x + _y)
-    _x *= alph
-    _y *= alph
+    alpha = 1 / (_x + _y)
+    _x *= alpha
+    _y *= alpha
     xt = (_x, _y)
-    
     return xt
 
 
@@ -22,13 +21,17 @@ def hmm(probs, evidence):
     d = (probs[3], True)
     f = (probs[4], False)
     e = [evidence[0], evidence[1]]
-    x1 = filtering(x0, b, c, d, f, e)
-    x2 = filtering(x1, b, c, d, f, e)
-    #  filtering stuff below
-
     # print(e)
-    # print("0.5,0.7,0.3,0.9,0.2,t,t--><0.8834,0.1166>")
-    # print("0.5,0.7,0.3,0.9,0.2,t,t,f--><0.1907,0.8093>")
+    #  filtering stuff below
+    x1 = filtering(x0, b, c, d, f, e)
+    print(f"{probs[0]},{probs[1]},{probs[2]},{probs[3]},{probs[4]},{e[0]},{e[1]}--><{x1[0]:.4f},{x1[1]:.4f}>")
+    x2 = filtering(x1, b, c, d, f, e)
+    state = max(x2[0], x2[1])
+    if state == x2[0]:
+        e.append('t')
+    elif state == x2[1]:
+        e.append('f')
+    print(f"{probs[0]},{probs[1]},{probs[2]},{probs[3]},{probs[4]},{e[0]},{e[1]},{e[2]}--><{x2[0]:.4f},{x2[1]:.4f}>")
 
 
 #  ['0.5', '0.7', '0.3', '0.9', '0.2', 't', 't', '0.7', '0.8', '0.3', '0.2', '0.7', 't', 'f']
@@ -49,9 +52,9 @@ def main(args):
     for i in content:
         if type(i) is str:
             if i == 't':
-                eValues.append(True)
+                eValues.append('t')
             elif i == 'f':
-                eValues.append(False)
+                eValues.append('f')
             else:
                 probs.append(i)
 
